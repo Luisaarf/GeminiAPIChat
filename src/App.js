0686a6b4f29985364137de2b8e7baddffcee1d08
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-05-08 08:23:46 -0300

    feat: front-end input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import {useState} from 'react';
+import './index.css';
 
 function App() {
+  const [valueInput, setValueInput] = useState('')
+  const [error, setError] = useState('')
+
   return (
     <div className="App">
-      <section className='search-section'>
-        
-      </section>
+        <p>Chat with me</p>
+      <div className='input-container'>
+        <input 
+          value={valueInput}
+          type='text' 
+          placeholder='Hello there!' 
+          onChange={(e) => setValueInput(e.target.value)}
+        />
+        {!error && <button>Send</button>}
+        {error && <button>Clear</button>}
+      </div>
+      {error && <p className='error-message'>{error}</p>}
+      <div className='search-result'>
+        <div key={''}>
+          <p className='answer'></p>
+
+        </div>
+      </div>
     </div>
   );
 }
