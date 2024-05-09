17e6179be47c4ceeb3651aff3f9fb04c3a53b7bb
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-05-08 23:25:16 -0300

    feat: chat working

--- server.js
+++ server.js
@@ -10,46 +10,36 @@ require('dotenv').config();
 
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
-const genAI = new GoogleGenerativeAI(process.env.GENERATIVE_AI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEN_API_KEY);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
 app.post('/gemini', async (req, res) => {
-    // req.body.history = req.body.history.map((item) => {});
-    console.log(req.body.history);
-    console.log(req.body.message);
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro'});
+    const chat = model.startChat({
+        history: req.body.history,
+        generationConfig: {
+            maxOutputTokens: 100,
+        },
+    });
+    const msg = req.body.message;
+    const result = await chat.sendMessage(msg);
+    const response = await result.response;
+    const text = response.text();
+    res.send(text);
 });
 
 
 
-// async function run() {
-//   // For text-only input, use the gemini-pro model
-//   const model = genAI.getGenerativeModel({ model: 'gemini-pro'});
-
-//   const chat = model.startChat({
-//     history: [
-//       {
-//         role: 'user',
-//         parts: [{ text: 'Hello, I have 2 dogs in my house.' }],
-//       },
-//       {
-//         role: 'model',
-//         parts: [{ text: 'Great to meet you. What would you like to know?' }],
-//       },
-//     ],
-//     generationConfig: {
-//       maxOutputTokens: 100,
+// [
+//     {
+//       role: 'user',
+//       parts: [{ text: 'Hello, I have 2 dogs in my house.' }],
 //     },
-//   });
-
-//   const msg = 'How many paws are in my house?';
-
-//   const result = await chat.sendMessage(msg);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-
-// run();
+//     {
+//       role: 'model',
+//       parts: [{ text: 'Great to meet you. What would you like to know?' }],
+//     },
+//   ]
--- src/App.js
+++ src/App.js
@@ -4,6 +4,45 @@ import './index.css';
 function App() {
   const [valueInput, setValueInput] = useState('')
   const [error, setError] = useState('')
+  const [chatHistory, setChatHistory] = useState([])
+
+  const getResponse = async (message) => {
+    if (!valueInput){
+      setError('Please enter a message');
+      return; 
+    }
+    try{
+
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          history: chatHistory,
+          message: valueInput
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/gemini', options);
+      const data = await response.text();
+      console.log('data:', data)
+      setChatHistory( oldChatHistory => [...oldChatHistory, 
+        {role: 'user', parts: [{text : valueInput} ] }, 
+        {role: 'model', parts: [{text : data} ]} 
+      ])
+      setValueInput('')
+
+    }catch(error){
+      console.error('error:', error)
+      setError('Something went wrong :( Please try again later.')
+    }
+  }
+
+  const clear = () => {
+    setValueInput('')
+    setError('')
+    setChatHistory([])
+  }
 
   return (
     <div className="App">
@@ -15,15 +54,21 @@ function App() {
           placeholder='Hello there!' 
           onChange={(e) => setValueInput(e.target.value)}
         />
-        {!error && <button>Send</button>}
-        {error && <button>Clear</button>}
+        {!error && <button onClick={getResponse}>Send</button>}
+        {error && <button onClick={clear}>Clear</button>}
       </div>
       {error && <p className='error-message'>{error}</p>}
       <div className='search-result'>
-        <div key={''}>
-          <p className='answer'></p>
-
-        </div>
+        {chatHistory.map((chatItem, _index) => {
+          {console.log('chatItem:', chatItem)
+            console.log('chatItem.role:', chatItem.role)
+            console.log('chatItem.parts[0].text:', chatItem.parts[0].text)
+          }
+          return (
+          <div key={_index}>
+            <p className='answer'>{chatItem.role} : {chatItem.parts[0].text}</p>
+          </div> )
+        })}
       </div>
     </div>
   );
